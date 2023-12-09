3938c3ff0c8edf1ae5c2518d09ffa45bf4cc27f6
eungyu1 <eungyu@letscoding.kr>
2023-12-09 11:48:56 +0900

    image 업로드

--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class feed(models.Model):
+    content = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='image/' ,max_length= 225, null=True , blank=True)
--- edu/views.py
+++ edu/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import View
 from django.shortcuts import render,redirect
-
+from .models import feed #현재폴더의 models라는 파일에서 Feed 라는 이름의 class를 가져오세요
 
 # Create your views here.
 class Index(View):
@@ -23,5 +23,8 @@ class NewContent(View):
     
     def post(self, request):
         param = request.POST.get('content','')
+        param2 = request.FILES.get('up_photo',False)
         print(f"param:(param)")
+        f = feed(content=param,photo=param2)
+        f.save()
         return redirect('edu:tag_study')
